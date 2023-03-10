644c16eda92b89adc4e64bb4720fd1fb89df1bca
Loc Nguyen <loc.nguyen@saritasa.com>
2023-03-10 15:22:39 +0700

    course 5 week 3 admin site

---
+++ course05-django-web-framework/week03/admin/workplace/myproject/myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    role = models.CharField(max_length=100)
+    shift = models.IntegerField()
+
+
