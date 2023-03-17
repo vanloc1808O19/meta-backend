585ab48ff266c13a66ff91b8c3e05b9eb79a6c9a
Loc Nguyen <loc.nguyen@saritasa.com>
2023-03-17 11:11:26 +0700

    w2 l2

---
+++ course06-apis/week02-lab02/LittleLemon/LittleLemonDRF/serializers.py
@@ -0,0 +1,16 @@
+from .models import MenuItem
+from rest_framework import serializers
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ["id", "title", "price", "inventory"]
+        extra_kwargs = {
+            "price": {
+                "min_value": 2,
+            },
+            "inventory": {
+                    "min_value": 0,
+            },
+        }
---
+++ course06-apis/week02-lab02/LittleLemon/LittleLemonDRF/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from rest_framework import generics
+
+# Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+
